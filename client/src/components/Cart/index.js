ece61ab3f5e5b0381fa53eb920e43c33a22266e9
zanderson1998 <zanderson1998@yahoo.com>
2021-04-17 16:09:17 -0600

    add offline capabilities

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+
+import { idbPromise } from "../../utils/helpers";
 
 const Cart = () => {
 
     const [state, dispatch] = useStoreContext();
+
+    useEffect(() => {
+        async function getCart() {
+            const cart = await idbPromise('cart', 'get');
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+        };
+
+        if (!state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
+
 
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
@@ -44,20 +58,20 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ?
-                            <button>
-                                Checkout
+                                <button>
+                                    Checkout
                             </button>
-                            :
-                            <span> (log in to check out)</span>
+                                :
+                                <span> (log in to check out)</span>
                         }
                     </div>
                 </div>
             ) : (
-                <h3>
-                    <span role="img" aria-label="shocked">😱</span>
+                    <h3>
+                        <span role="img" aria-label="shocked">😱</span>
                     You haven't added anything to your cart yet!
-                </h3>
-            )}
+                    </h3>
+                )}
         </div>
     );
 };
--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -2,32 +2,39 @@ import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
+import { idbPromise } from "../../utils/helpers";
+
 const CartItem = ({ item }) => {
 
     const [, dispatch] = useStoreContext();
 
     const removeFromCart = item => {
         dispatch({
-            type: REMOVE_FROM_CART,
-            _id: item._id
+          type: REMOVE_FROM_CART,
+          _id: item._id
         });
-    };
+        idbPromise('cart', 'delete', { ...item });
+      };
 
     const onChange = (e) => {
         const value = e.target.value;
 
-        if (value ==="0") {
+        if (value === '0') {
             dispatch({
-                type: REMOVE_FROM_CART,
-                _id: item._id
+              type: REMOVE_FROM_CART,
+              _id: item._id
             });
-        } else {
+          
+            idbPromise('cart', 'delete', { ...item });
+          } else {
             dispatch({
-                type: UPDATE_CART_QUANTITY,
-                _id: item._id,
-                purchaseQuantity: parseInt(value)
+              type: UPDATE_CART_QUANTITY,
+              _id: item._id,
+              purchaseQuantity: parseInt(value)
             });
-        }
+          
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+          }
     };
 
     return (

5fc4f978f22328305fa955e74d975be4c809e3ba
zanderson1998 <zanderson1998@yahoo.com>
2021-04-18 12:05:54 -0600

    finish stripe setup

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 import OrderHistory from "./pages/OrderHistory";
+import Success from './pages/Success';
 
 import { StoreProvider } from './utils/GlobalState';
 
@@ -38,6 +39,7 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -7,9 +7,19 @@ import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 
 import { idbPromise } from "../../utils/helpers";
 
+//stripe imports
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 const Cart = () => {
 
     const [state, dispatch] = useStoreContext();
+
+    //stripe checkout query setup
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
     useEffect(() => {
         async function getCart() {
@@ -35,6 +45,30 @@ const Cart = () => {
         return sum.toFixed(2);
     }
 
+    //stripe checkout
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+        });
+    }
+
+    //watch for stripe checkout data to change and redirect to checkout session
+    useEffect(() => {
+        if(data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session });
+            });
+        }
+    }, [data]);
+
     if (!state.cartOpen) {
         return (
             <div className="cart-closed" onClick={toggleCart}>
@@ -58,9 +92,9 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ?
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
-                            </button>
+                                </button>
                                 :
                                 <span> (log in to check out)</span>
                         }
